ec8ea23181441f7e63d55afd184ae64060e3019c
Marek Tucani <marek.tucani@kistler.com>
2016-01-08 13:04:07 +0100

    added vs-repeat-if documentation and made it optional attribute

--- src/angular-vs-repeat.js
+++ src/angular-vs-repeat.js
@@ -66,6 +66,7 @@
     // vs-size - a property name of the items in collection that is a number denoting the element size (in pixels)
     // vs-autoresize - use this attribute without vs-size and without specifying element's size. The automatically computed element style will
     //              readjust upon window resize if the size is dependable on the viewport size
+    // vs-repeat-if - contains condition, angular-vs-repeat will be applied only when true
 
     // EVENTS:
     // - 'vsRepeatTrigger' - an event the directive listens for to manually trigger reinitialization
@@ -198,7 +199,7 @@
                 $element.empty();
                 return {
                     pre: function($scope, $element, $attrs) {
-                        if (!$scope.$eval($attrs.vsRepeatIf)){
+                        if ($attrs.vsRepeatIf && !$scope.$eval($attrs.vsRepeatIf)){
                             //evaluate condition if angular-vs-repeat should be applied
                             $element.append($compile(ngRepeatChildren)($scope));
                             return;
